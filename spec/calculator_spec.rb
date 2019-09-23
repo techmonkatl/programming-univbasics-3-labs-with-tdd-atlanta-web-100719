e60ece8d62e9285c56b29b418d186a745691e9b5
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 19:50:57 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,7 +9,7 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    second_number = 2('./calculator.rb', "second_number")
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
