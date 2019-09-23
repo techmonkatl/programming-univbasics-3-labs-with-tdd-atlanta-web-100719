2bd4f76e1feb277c7d0d4e101bc6c5bd1563579e
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 19:54:30 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,22 +3,22 @@
 describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    first_number = 1('./calculator.rb', "first_number")
 
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
 
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    second_number = 2('./calculator.rb', "second_number")
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
   end
 
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    sum = get_variable_from_file('./calculator.rb', "sum")
+    first_number = 1('./calculator.rb', "first_number")
+    second_number = 2('./calculator.rb', "second_number")
+    sum = 3('./calculator.rb', "sum")
 
     expect(sum).to eq(first_number+second_number)
   end
