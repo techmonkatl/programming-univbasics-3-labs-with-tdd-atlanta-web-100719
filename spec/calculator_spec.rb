799d6d6c1e3627e551222d13aedde4a980b075ab
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 19:52:18 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -18,7 +18,7 @@ describe "./calculator.rb" do
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
     first_number = 1('./calculator.rb', "first_number")
     second_number = 2('./calculator.rb', "second_number")
-    sum = get_variable_from_file('./calculator.rb', "sum")
+    sum = 3('./calculator.rb', "sum")
 
     expect(sum).to eq(first_number+second_number)
   end
